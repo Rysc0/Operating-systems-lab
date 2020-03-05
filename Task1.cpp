d3b0ee5e5d5950f1129395d926fe895af6e3f66f
Rysc0 <boris.bastek@gmail.com>
2020-03-05 23:02:40 +0100

    Initial Commit

---
+++ Task1.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<signal.h>
+#include<unistd.h>
+using namespace std;
+
+void obrada_prekida(int j){
+  cout << "Start --- obrada prekida" << endl;
+  for(int i=1;i<=10;i++){
+    cout << "Prekid " << i << "/10" << endl;
+    sleep(1);
+  }
+  sleep(j);
+  cout << "End --- obrada gotova" << endl;
+}
+//https://github.com/Rysc0/Operating-systems-lab
+void prekidna_rutina(int sig){
+  int i;
+
+  cout << "Prekidna rutina aktivirana!" << endl;
+
+  switch (sig) {
+    case SIGINT:
+        i = 1;
+        break;
+    case SIGQUIT:
+      i = 2;
+      break;
+    case SIGTSTP:
+      i = 3;
+      break;
+    default:
+      return;
+  }
+  cout << "Razina prekida: " << i << endl;
+}
+
+int main(void){
+  sigset(SIGINT, prekidna_rutina);
+  sigset(SIGQUIT, prekidna_rutina);
+  sigset(SIGTSTP, prekidna_rutina);
+
+  cout << "Osnovni program --- START" << endl;
+  for(int i=1;i<=20;i++){
+    cout << i << "/20" << endl;
+    sleep(1);
+  }
+  cout << "Osnovni program --- END" << endl;
+  return 0;
+  }
