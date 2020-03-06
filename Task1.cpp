456f61efab7a82094fef1b0c2b5a6a7c13d61f6a
Rysc0 <boris.bastek@gmail.com>
2020-03-06 20:39:16 +0100

    Started implementing priority system

--- Task1.cpp
+++ Task1.cpp
@@ -4,20 +4,25 @@
 using namespace std;
 
 void obrada_prekida(int j){
-  cout << "Start --- obrada prekida" << endl;
-  for(int i=1;i<=10;i++){
-    cout << "Prekid " << i << "/10" << endl;
-    sleep(1);
-  }
-  sleep(j);
+  int prioritet = 0;
+  if(j>prioritet){
+    prioritet = j;
+    cout << "Razina prekida = " << prioritet << j << endl;
+    cout << "Start --- obrada prekida" << endl;
+    for(int i=1;i<=5;i++){
+      cout << "Prekid " << i << "/5" << endl;
+      sleep(1);
+    }
+  //  sleep(j);
   cout << "End --- obrada gotova" << endl;
-  cout << j << endl;
+  //cout << j << endl;
+  }
+  else return;
 }
-
 void prekidna_rutina(int sig){
   int i;
 
-  cout << "Prekidna rutina aktivirana!" << endl;
+  cout << "Prekidna rutina aktivirana!" << endl << endl;
   switch (sig) {
     case SIGINT:
         i = 1;
@@ -34,7 +39,7 @@ void prekidna_rutina(int sig){
     default:
       return;
   }
-  cout << "Razina prekida: " << i << endl;
+  //cout << "Razina prekida: " << i << endl;
 }
 
 int main(void){
