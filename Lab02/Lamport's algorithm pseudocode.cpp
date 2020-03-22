1c5b033e2fe574499899d5428e944673e1b88721
Rysc0 <boris.bastek@gmail.com>
2020-03-22 21:27:49 +0100

    Pseudocode for Lamport's algorithm

---
+++ Lab02/Lamport's algorithm pseudocode.cpp
@@ -0,0 +1,42 @@
+/*Ostvariti sustav paralelnih procesa/dretava. Struktura procesa/dretava dana je
+sljedećim pseudokodom:*/
+
+proces proc(i)          /* i  [0..n-1] */
+{
+   za k = 1 do 5 čini {
+      uđi u kritični odsječak
+      za m = 1 do 5 čini {
+          ispiši (i, k, m)
+      }
+      izađi iz kritičnog odsječka
+   }
+}
+
+zajedničke varijable: TRAŽIM[0..n-1], BROJ[0..n-1]
+
+funkcija uđi_u_kritični_odsječak(i)
+{
+   TRAŽIM[i] = 1
+   BROJ[i] = max(BROJ[0],...,BROJ[n-1]) + 1
+   TRAŽIM[i] = 0
+
+   za j = 0 do n-1 čini
+      dok je TRAŽIM[j] <> 0 čini
+         ništa
+      dok je BROJ[j] <> 0 && (BROJ[j] < BROJ[i] || (BROJ[j] == BROJ[i] && j < i)) čini
+         ništa
+}
+
+funkcija izađi_iz_kritičnog_odsječka(i)
+{
+   BROJ[i] = 0
+}
+
+/* Ako se program rješava s procesima tada treba zajedničke varijable tako organizirati
+da se prostor za njih zauzme odjednom i podijeli među njima. Ovo je nužno zbog ograničenog
+broja segmenata i velikog broja korisnika.*/
+
+Ovisno o opterećenju računala i broju procesa koji se pokreću, a da bi se vidjele razlike prilikom izvođenja programa može biti potrebno usporiti izvršavanje sa:
+sleep(1);
+
+nakon: ispisi (i, k, m).
