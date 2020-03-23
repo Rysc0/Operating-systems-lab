443200d437cd59cf606fd758d084e404807a0a9f
Rysc0 <boris.bastek@gmail.com>
2020-03-23 10:38:15 +0100

    Pseudocode setup

--- Lab02/Lamport's algorithm pseudocode.cpp
+++ Lab02/Lamport's algorithm pseudocode.cpp
@@ -1,42 +1,52 @@
 /*Ostvariti sustav paralelnih procesa/dretava. Struktura procesa/dretava dana je
 sljedećim pseudokodom:*/
-
-proces proc(i)          /* i  [0..n-1] */
-{
-   za k = 1 do 5 čini {
-      uđi u kritični odsječak
-      za m = 1 do 5 čini {
-          ispiši (i, k, m)
-      }
-      izađi iz kritičnog odsječka
-   }
-}
+#include<iostream>
+#include<cstdlib>        /*exit() */
+#include<unistd.h>      /*fork()*/
+#include<sys/wait.h>  /* wait()*/
+using namespace std;
 
 zajedničke varijable: TRAŽIM[0..n-1], BROJ[0..n-1]
 
 funkcija uđi_u_kritični_odsječak(i)
 {
-   TRAŽIM[i] = 1
-   BROJ[i] = max(BROJ[0],...,BROJ[n-1]) + 1
-   TRAŽIM[i] = 0
+  TRAŽIM[i] = 1
+  BROJ[i] = max(BROJ[0],...,BROJ[n-1]) + 1
+  TRAŽIM[i] = 0
 
-   za j = 0 do n-1 čini
-      dok je TRAŽIM[j] <> 0 čini
-         ništa
-      dok je BROJ[j] <> 0 && (BROJ[j] < BROJ[i] || (BROJ[j] == BROJ[i] && j < i)) čini
-         ništa
+  za j = 0 do n-1 čini
+  dok je TRAŽIM[j] <> 0 čini
+  ništa
+  dok je BROJ[j] <> 0 && (BROJ[j] < BROJ[i] || (BROJ[j] == BROJ[i] && j < i)) čini
+  ništa
 }
 
 funkcija izađi_iz_kritičnog_odsječka(i)
 {
-   BROJ[i] = 0
+  BROJ[i] = 0
 }
 
-/* Ako se program rješava s procesima tada treba zajedničke varijable tako organizirati
-da se prostor za njih zauzme odjednom i podijeli među njima. Ovo je nužno zbog ograničenog
-broja segmenata i velikog broja korisnika.*/
+int main(){
 
-Ovisno o opterećenju računala i broju procesa koji se pokreću, a da bi se vidjele razlike prilikom izvođenja programa može biti potrebno usporiti izvršavanje sa:
-sleep(1);
+  proces proc(i)          /* i  [0..n-1] */
+  {
+    za k = 1 do 5 čini {
+      uđi u kritični odsječak
+      za m = 1 do 5 čini {
+        ispiši (i, k, m)
+      }
+      izađi iz kritičnog odsječka
+    }
+  }
 
-nakon: ispisi (i, k, m).
+  /* Ako se program rješava s procesima tada treba zajedničke varijable tako organizirati
+  da se prostor za njih zauzme odjednom i podijeli među njima. Ovo je nužno zbog ograničenog
+  broja segmenata i velikog broja korisnika.*/
+
+  Ovisno o opterećenju računala i broju procesa koji se pokreću, a da bi se vidjele razlike prilikom izvođenja programa može biti potrebno usporiti izvršavanje sa:
+  sleep(1);
+
+  nakon: ispisi (i, k, m).
+
+  return 0;
+}
---
+++ Lab02/Lamport's algorithm.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdlib>        /*exit() */
+#include<unistd.h>      /*fork()*/
+#include<sys/wait.h>  /* wait()*/
+
+int main(){
+
+
+
+
+
+  return 0;
+}
