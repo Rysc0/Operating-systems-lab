1aa74b8693c9ca9e63ff15f218ee4ab678e990b4
Rysc0 <boris.bastek@gmail.com>
2020-03-27 15:55:26 +0100

    Implemented shared memory. Need to work on Lamport now.

--- Lab02/moodle.cpp
+++ Lab02/moodle.cpp
@@ -12,19 +12,11 @@ sljedećim pseudokodom:*/
 #include <sys/wait.h>  /* wait()*/
 using namespace std;
 
-int *ptr,*ptr1,*Nprocesa;
+int *array, id;
+/*
+void kriticni_odsjecak(i){
 
-void f(int polje[], int size){
-  cout << "Ide polje" << endl;
-  for(int i=0;i<5;i++){
-    ptr[i] = 0;
-    cout << ptr[i] << endl;
-  }
-  cout << "Idu PID-ovi" << endl;
-  for(int i = 0; i<size;i++){
-    cout << polje[i] << endl;
-  }
-}
+}*/
 
 void funkcija(){
   printf("\x1B[31mNEXT PROCCESS...\033[0m\n");
@@ -33,45 +25,45 @@ void funkcija(){
 int main(int argc, char **argv) {
 
   int procesa = atoi(argv[1]);
-  int array[procesa];
-  int pid[procesa];
-  ptr = array;
-  ptr1 = pid;
-  //Nprocesa = procesa;
+
+  int polje[10];
+  array = polje;
+
+  id = shmget(IPC_PRIVATE,sizeof(int)*10,0600);
+  if(id == -1){
+    cout << "ERROR, NO SHARED MEMORY!" << endl;
+    exit(1);
+  }
+
+  array = (int*) shmat(id,NULL,0);
+
   // STARTING PARALEL PROCCESES
   for(int i = 0; i < procesa; i++){
     if (fork() == 0) {
       // child code
-      pid[i] = getpid();
-      cout << pid[i] << endl;
       cout << "I'm child proccess with pid: " << getpid() << endl;
-      //for(int i = 0; i < procesa; i++){
-        funkcija();
-        //sleep(1);
-        for(int k = 1; k <=5; k++){
-          //uđi_u_kritični_odsječak(k);
-          for(int m = 1; m <=5; m++){
-            //ispisi(i,k,m);
-            cout << "Proces " << i << " K.O. br: " << k << " (" << m << "/5)" << endl;
-          }
-          // izađi iz kritičnog odsječka
+      funkcija();
+
+      for(int k = 1; k <=5; k++){
+        //uđi_u_kritični_odsječak(k);
+        for(int m = 1; m <=5; m++){
+          //ispisi(i,k,m);
+          cout << "Proces " << i << " K.O. br: " << k << " (" << m << "/5)" << endl;
         }
-      //}
+        // izađi iz kritičnog odsječka
+      }
       exit(0);
     }
   }
-  /*cout << "Resavam PIDove: " << endl;
-  for(int i = 0; i < procesa; i++){
-    cout << pid[i] << endl;
-  //}
-*/
+
   sleep(2);
   for(int i = 0; i < procesa; i++){
     cout << "Waited on proccess " << i << endl;
     wait(NULL);
   }
-  // f(pid,procesa);
 
+  shmdt(array);
+  shmctl(id,IPC_RMID,NULL);
 
 
 
