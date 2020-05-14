7ec59d88565cd0c53ecf4af5faf19205df781218
Rysc0 <boris.bastek@gmail.com>
2020-05-14 22:41:09 +0200

    Final version

--- Lab03/jebiga.cpp
+++ Lab03/jebiga.cpp
@@ -16,11 +16,11 @@
 using namespace std;
 
 
-// struktura semafora + memorija
+// semafori + buffer
 struct memorija{
   int ULAZ, IZLAZ, BUFFER[100];
-  sem_t semafor1, semafor2;
-  // semafori - 1 za pristup bufferu, 1 za komunikaciju
+  sem_t semafor, semafor2;
+  
 }
 
 *ptr = new memorija;
@@ -28,25 +28,24 @@ struct memorija{
 int shared_memory_id;
 
 void generator(int n_brojeva){
-   //dok (nije generirano n zadataka){
-  do{
+  
+  for(int i = 0; i < n_brojeva; i++){
     ptr->BUFFER[ptr->ULAZ] = rand()%100000000;
-    cout << "Generiran broj: " << ptr->BUFFER[ptr->ULAZ] << endl;
-    sem_post(&ptr->semafor1);
+    cout << "Generirani broj: " << ptr->BUFFER[ptr->ULAZ] << endl;
+    sem_post(&ptr->semafor);
     sem_wait(&ptr->semafor2);
-    n_brojeva--;
-  }while(n_brojeva>0);
+  }  
 }
+
 
 void calculator(int number,int n_brojeva){
 
-  long int zbroj = 0; // veliki su brojevi
-  do{
-    sem_wait(&ptr->semafor1);
+  long int zbroj = 0; 
 
+  do{
+    sem_wait(&ptr->semafor);
     int procitani_broj = ptr->BUFFER[ptr->IZLAZ];
     cout << "Proces " << number << " je preuzeo broj/zadatak " << ptr->BUFFER[ptr->IZLAZ] << endl;
-    
     sem_post(&ptr->semafor2);
 
     for(int i = 0; i < procitani_broj; i++){
@@ -67,15 +66,13 @@ void brisi_memoriju(int sig){
 }
 
 void unisti_semafore(){
-  sem_destroy(&ptr->semafor1);
+  sem_destroy(&ptr->semafor);
   sem_destroy(&ptr->semafor2);
 }
 
 
 
 
-
-// glavni program (parametri) 
 int main(int argc, char *argv[]){
 
 if(argc != 3){
@@ -87,9 +84,12 @@ srand(time(NULL));
 
 int m_procesa = atoi(argv[1]);
 int n_brojeva = atoi(argv[2]);
+
+if(((m_procesa<=0)||(n_brojeva<=0))||((m_procesa<=0)&&(n_brojeva<=0))){
+    cout << "Input arguments not in scope!" << endl;
+    exit(-1);
+  }
   
-cout << "Proces generator je započeo s radom!" << endl;
-cout << "Treba generirati " << n_brojeva << "brojeva/zadataka!" << endl;
 
 // napravi dijeljenu memoriju
 shared_memory_id = shmget(IPC_PRIVATE,sizeof(ptr),0600); 
@@ -103,7 +103,7 @@ sigset(SIGINT,brisi_memoriju);
 ptr = (memorija*) shmat(shared_memory_id,NULL,0);
 
 // inicijalizacija semafora
-int sem_id1 = sem_init(&ptr->semafor1,1,0);
+int sem_id1 = sem_init(&ptr->semafor,1,0);
 if(sem_id1 != 0){
   cout << "nema semafora 1" << endl;
 }
@@ -119,6 +119,7 @@ ptr->IZLAZ = 0;
 
 // pokreni proces generator
 if(fork()==0){
+  cout << "START ----> GENERATOR ----> " << n_brojeva << " TASKS NEEDED!" << endl;
   generator(n_brojeva);
   exit(1);
 }
@@ -126,7 +127,7 @@ if(fork()==0){
 //pokreni m procesa racunaj;   
 for(int i = 0; i < m_procesa; i++){
   if(fork() == 0){
-    cout << "Proces " << i << " je startao!" << endl;
+    cout << "START ----> PROCCESS " << i << endl;
     calculator(i,n_brojeva);      
     exit(1);
   }
@@ -139,7 +140,9 @@ while(n_brojeva>=0){
 }
 sleep(1);
 
-// brise dijeljenu memoriju
+
+
+// brise dijeljenu memoriju i semafore
 brisi_memoriju(SIGINT);
 unisti_semafore();
 
