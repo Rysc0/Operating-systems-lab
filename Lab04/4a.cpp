003a0132309f4accdc3d8a7fb11edcea58400947
Rysc0 <boris.bastek@gmail.com>
2020-06-02 20:57:28 +0200

    idk, ne radi

--- Lab04/4a.cpp
+++ Lab04/4a.cpp
@@ -6,42 +6,40 @@
 #include <stdlib.h>
 using namespace std;
 
-int broj;
-int counter;
-int *brojac,*n;
+
+int counter = 0,broj;
+int *brojac,*n,*red;
 pthread_mutex_t lock;
 
 
 void barijera(pthread_mutex_t lock,int array[]){
   pthread_mutex_lock(&lock);
-  counter++;
-  if(br<n){
-    int pthread_cond_wait(pthread_cond_t *counter, pthread_mutex_t *lock);
+  
+  
+  counter = counter +1;
+  if(counter <= *n){
+    int pthread_cond_wait(pthread_cond_t *array, pthread_mutex_t *lock);
   }
-
+  else{
+    counter = 0;
+     int pthread_cond_broadcast(pthread_cond_t *array);
+  }
   pthread_mutex_unlock(&lock);
 }
 
+
+
 void* thread_funkcija(void* arg){
-  pthread_mutex_lock(&lock);
+ 
+  barijera(lock, red);
+ 
   
-  cout << "Ja sam thread " << counter+1 << endl;
-  counter = counter +1;
-  cout << "Unesi broj: ";
-  cin >> broj;
-  if(brojac < n){
-    int pthread_cond_wait(pthread_cond_t *brojac, pthread_mutex_t *lock);
-  }
-  else{
-    brojac = 0;
-    int pthread_cond_broadcast(pthread_cond_t *brojac);
-  }
-  cout << "Counter je: " << brojac << endl;
-  pthread_mutex_unlock(&lock);
-  cout << "Broj: " << broj << endl; 
   pthread_exit(0);
   
 }
+
+
+
 
 
 int main(int argc, char *argv[]){
@@ -49,11 +47,12 @@ int main(int argc, char *argv[]){
 
 int prvi_argument = atoi(argv[1]);
 int array[prvi_argument];
+red = array;
 pthread_t thread_id[prvi_argument];
-int vrijednost = 1;
-brojac = &vrijednost;
+
 n = &prvi_argument;
-int pthread_cond_init(pthread_cond_t *brojac, const pthread_condattr_t __NULL);
+
+int pthread_cond_init(pthread_cond_t *array, const pthread_condattr_t __NULL);
 
 if (pthread_mutex_init(&lock, NULL) != 0) { 
   cout << "\n mutex init has failed\n" << endl; 
@@ -71,8 +70,7 @@ for(int i = 0; i < prvi_argument; i++){
 for(int i = 0; i < prvi_argument; i++){
   pthread_join(thread_id[i],NULL);
 }
-
-
+int pthread_cond_destroy(*array);
 pthread_mutex_destroy(&lock);
 
   return 0;
