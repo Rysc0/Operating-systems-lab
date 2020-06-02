e17f972ac548374be86275904d8ad93f813be754
Rysc0 <boris.bastek@gmail.com>
2020-06-02 20:05:28 +0200

    Barijera u razvoju...nista ne radi

--- Lab04/4a.cpp
+++ Lab04/4a.cpp
@@ -6,20 +6,41 @@
 #include <stdlib.h>
 using namespace std;
 
-
+int broj;
 int counter;
-
+int *brojac,*n;
 pthread_mutex_t lock;
+
+
+void barijera(pthread_mutex_t lock,int array[]){
+  pthread_mutex_lock(&lock);
+  counter++;
+  if(br<n){
+    int pthread_cond_wait(pthread_cond_t *counter, pthread_mutex_t *lock);
+  }
+
+  pthread_mutex_unlock(&lock);
+}
 
 void* thread_funkcija(void* arg){
   pthread_mutex_lock(&lock);
   
   cout << "Ja sam thread " << counter+1 << endl;
   counter = counter +1;
-
+  cout << "Unesi broj: ";
+  cin >> broj;
+  if(brojac < n){
+    int pthread_cond_wait(pthread_cond_t *brojac, pthread_mutex_t *lock);
+  }
+  else{
+    brojac = 0;
+    int pthread_cond_broadcast(pthread_cond_t *brojac);
+  }
+  cout << "Counter je: " << brojac << endl;
   pthread_mutex_unlock(&lock);
+  cout << "Broj: " << broj << endl; 
   pthread_exit(0);
-  // return NULL;
+  
 }
 
 
@@ -27,7 +48,12 @@ int main(int argc, char *argv[]){
   
 
 int prvi_argument = atoi(argv[1]);
+int array[prvi_argument];
 pthread_t thread_id[prvi_argument];
+int vrijednost = 1;
+brojac = &vrijednost;
+n = &prvi_argument;
+int pthread_cond_init(pthread_cond_t *brojac, const pthread_condattr_t __NULL);
 
 if (pthread_mutex_init(&lock, NULL) != 0) { 
   cout << "\n mutex init has failed\n" << endl; 
